44e28fdc9dbce97d9af0d82e504589717df1529b
Aby Abraham <abyabrkal@gmail.com>
2019-10-22 16:10:09 +0400

    Table & Button -> Class to Classless Functional components

--- src/App.js
+++ src/App.js
@@ -88,49 +88,37 @@ const Search = ({ value, onChange, children }) =>
       </form>
 
 
-class Table extends React.Component {
-  render() {
-    const { list, pattern, onDismiss } = this.props;
 
-    console.log(this.props)
-    return (
-      <div>
-      { list.filter(isSearched(pattern)).map(item => 
-        <div key={item.objectID}>
-          <span>
-            <a href={item.url}>{item.title}</a>
-          </span>
-          <span>{item.author}</span>
-          <span>{item.num_comments}</span>
-          <span>{item.points}</span>
-          <span>
-            <Button
-              onClick={() => onDismiss(item.objectID)}
-            >
-              Dismiss
-            </Button>
-          </span>
-        </div>
-      )}
-      </div>
-    );
-  }
-}
+const Table = ({ list, pattern, onDismiss }) =>
+  <div>
+  { list.filter(isSearched(pattern)).map(item => 
+    <div key={item.objectID}>
+      <span>
+        <a href={item.url}>{item.title}</a>
+      </span>
+      <span>{item.author}</span>
+      <span>{item.num_comments}</span>
+      <span>{item.points}</span>
+      <span>
+        <Button
+          onClick={() => onDismiss(item.objectID)}
+        >
+          Dismiss
+        </Button>
+      </span>
+    </div>
+  )}
+  </div>
 
 
-class Button extends React.Component {
-  render() {
-    const {onClick, className='', children} = this.props;
+const Button = ({ onClick, className='', children }) => 
+  <button
+    onClick={onClick}
+    className={className}
+    type="button"
+  >{children}</button>
 
-    return (
-      <button
-        onClick={onClick}
-        className={className}
-        type="button"
-      >{children}</button>
-    )
-  }
-}
+
 
 export default App;
 
