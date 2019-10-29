9791925d4949f964610baf43a4e525a02eeda5b5
ABY ABRAHAM KALATHIVILA <abyabrkal@gmail.com>
2019-10-29 16:09:50 +0400

    Data Fetched from HackerNews API

--- src/App.js
+++ src/App.js
@@ -2,25 +2,10 @@ import React from 'react';
 import './index.css';
 
 
-const list = [
-  {
-    title: "React",
-    url: "https://reactjs.org/",
-    author: "Jordan Walke",
-    num_comments: 3,
-    points: 4,
-    objectID: 0
-  },
-  {
-    title: "Redux",
-    url: "https://redux.js.org/",
-    author: "Dan Abramov, Andrew Clark",
-    num_comments: 2,
-    points: 5,
-    objectID: 1
-  }
-];
-
+const DEFAULT_QUERY = 'redux';
+const PATH_BASE = 'https://hn.algolia.com/api/v1';
+const PATH_SEARCH = '/search';
+const PARAM_SEARCH = 'query=';
 
 
 const isSearched = (searchTerm) => (item) =>
@@ -34,10 +19,11 @@ class App extends React.Component {
     super(props);
 
     this.state = {
-      list, 
-      searchTerm: ''
+      result: null, 
+      searchTerm: DEFAULT_QUERY,
     };
 
+    this.setSearchTopStories = this.setSearchTopStories.bind(this);
     this.onDismiss = this.onDismiss.bind(this);
     this.onSearchChange = this.onSearchChange.bind(this);
   }
@@ -50,12 +36,31 @@ class App extends React.Component {
     });
   }
 
+  setSearchTopStories(result) {
+    this.setState({ result });
+  }
+
+
   onSearchChange(event) {
     this.setState({ searchTerm: event.target.value });
   }
 
+  componentDidMount() {
+    const { searchTerm } = this.state;
+
+    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`)
+      .then(response => response.json())
+      .then(result => this.setSearchTopStories(result))
+      .catch(error => error)
+  }
+
+
+
   render() {
-    const { searchTerm, list } = this.state;
+    const { searchTerm, result } = this.state;
+    console.log(result)
+
+    if (!result) return null;
 
     return (
       <div className="page">
@@ -68,7 +73,7 @@ class App extends React.Component {
           <hr />
         </div>
         <Table 
-          list={list}
+          list={result.hits}
           pattern={searchTerm}
           onDismiss={this.onDismiss}
         />
